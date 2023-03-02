dbd7933823974643cc8c8a2e6dca2acc24e72f0e
BuildTools <unconfigured@null.spigotmc.org>
2023-03-02 10:52:54 +0100

    added pb-pull added pb-push

---
+++ pocketbase/pb-pull.js
@@ -0,0 +1,16 @@
+const { Client } = require('node-scp')
+
+if (process.env.server_ip == undefined || process.env.server_port == undefined || process.env.server_username == undefined || process.env.server_password == undefined) { console.log("Please set the server_ip, server_port, server_username, and server_password environment variables."); process.exit(1); }
+
+Client({
+    host: process.env.server_ip,
+    port: process.env.server_port,
+    username: process.env.server_username,
+    password: process.env.server_username,
+}).then(client => {
+    client.downloadDir('~/pocketbase/pb_data', './pocketbase/pb_data')
+        .then(response => {
+            client.close()
+        })
+        .catch(error => { })
+}).catch(e => console.log(e))

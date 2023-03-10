d89c94af404bf9d9b9ad5ef331256742e9442921
Max Erkel <max@erkel.me>
2023-03-10 14:20:55 +0100

    🔧redirect if logged in

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '@/styles/globals.css';
-import { DiscordContextProvider } from '@/util/DcContext';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
